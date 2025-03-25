05de1f6ce4e992fb060f26d6a14ad875fbd559e8
jchabert0 <jchabert0@gmail.com>
2025-03-25 13:58:06 +0100

    Ajout du nombre total d'écoute sur les cards des artistes

--- src/components/AppArtist.vue
+++ src/components/AppArtist.vue
@@ -1,39 +1,11 @@
 <template>
   <template v-if="artist">
+
     <AppArtistInfos :id="artist._id"/>
 
     <AppArtistPopularyTitles :id="artist._id"/>
 
     <AppArtistAlbums :id="artist._id"/>
-  
-<!--
-    <div class="row mb-4 border-bottom mb-4 pb-4">
-      <div class="col-lg-2">
-        <img src="http://localhost:8085/img/album/No-album-art.png" :alt="artist.prenom+' '+artist.nom" class="img-thumbnail">
-        <h5>
-          Intitulé de l'album
-          <span class="text-muted">05/11/2021</span>
-        </h5>
-      </div>
-      <div class="col-lg-7">
-        <table class="table">
-          <tbody>
-          <tr v-for="index in [0, 1 , 2, 4]" :key="index">
-            <td class="align-middle">
-              Titre de la chanson
-            </td>
-            <td class="text-right align-middle">
-              2:45
-            </td>
-            <td class="text-right align-middle">
-              1 548
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
--->
 
   </template>
 
@@ -73,8 +45,6 @@ export default {
   data() {
     return {
       artist: null,
-      artistsSimilaires: [],
-      titresPopulaires: []
     };
   },
   async mounted() {
@@ -84,61 +54,6 @@ export default {
       if(response.data[i]._id === this.id) {
         this.artist = response.data[i];
       }
-    }
-
-    this.artistsSimilaires = [];
-    for (var j in response.data) {
-      if(response.data[j].styleMusical === this.artist.styleMusical && response.data[j]._id !== this.artist._id) {
-        this.artistsSimilaires.push(response.data[j]);
-      }
-    }
-
-    this.titresPopulaires = this.mostListenedSongs();
-  },
-  methods: {
-    mostListenedSongs() {
-      return [
-        {
-          "_id": "618ae89fc40339e626d8dff2",
-          "numero": 0,
-          "titre": "reprehenderit in voluptate",
-          "dureeSecondes": 293,
-          "nombreEcoutes": 806453529,
-          "couverture": "http://localhost:8085/img/album/618ae89f89d64f81153d54c1.jpeg"
-        },
-        {
-          "_id": "618ae89ff18f04ae3de8ce14",
-          "numero": 1,
-          "titre": "minim",
-          "dureeSecondes": 231,
-          "nombreEcoutes": 129446587,
-          "couverture": "http://localhost:8085/img/album/618ae89f89d64f81153d54c1.jpeg"
-        },
-        {
-          "_id": "618ae89f771d425479ac8e65",
-          "numero": 2,
-          "titre": "tempor cupidatat",
-          "dureeSecondes": 239,
-          "nombreEcoutes": 741808849,
-          "couverture": "http://localhost:8085/img/album/618ae89f89d64f81153d54c1.jpeg"
-        },
-        {
-          "_id": "618ae89f506ca1120b835347",
-          "numero": 3,
-          "titre": "deserunt",
-          "dureeSecondes": 323,
-          "nombreEcoutes": 182839200,
-          "couverture": "http://localhost:8085/img/album/618ae89f89d64f81153d54c1.jpeg"
-        },
-        {
-          "_id": "618ae89f3cc86e1ea38d91f3",
-          "numero": 4,
-          "titre": "labore enim mollit",
-          "dureeSecondes": 270,
-          "nombreEcoutes": 374839288,
-          "couverture": "http://localhost:8085/img/album/618ae89f89d64f81153d54c1.jpeg"
-        }
-      ];
     }
   }
 };
--- src/components/ArtistCardForHome.vue
+++ src/components/ArtistCardForHome.vue
@@ -2,29 +2,49 @@
   <div class="col-sm-3 mg-b">
     <div class="card mb-4">
       <img class="card-img-top" :src="artist.photo" :alt="artist.prenom+' '+artist.nom">
-      <span class="text-white" style="padding: .75rem 1.25rem; margin-top:-2.35rem; background: linear-gradient(to top, black 0%, transparent 100%)">Nombre d'écoutes : <b >TODO</b></span>
+      <span class="text-white" style="padding: .75rem 1.25rem; margin-top:-2.35rem; background: linear-gradient(to top, black 0%, transparent 100%)">
+        Nombre d'écoutes : <b>{{ thousandsSeparator(totalEcoutes) }}</b>
+      </span>
       <div class="card-body">
         <h5 class="card-title">
-          {{artist.prenom}} {{artist.nom}}
-          <div class="text-muted">({{artist.age}} ans)</div>
+          {{ artist.prenom }} {{ artist.nom }}
+          <div class="text-muted">({{ artist.age }} ans)</div>
         </h5>
-        <p class="card-text custom-max-vh">{{artist.bio}}</p>
-        <router-link :to="{name: 'artist', params: {id: artist._id}}" class="btn btn-primary btn-block mt-2">Voir la page</router-link>
+        <p class="card-text custom-max-vh">{{ artist.bio }}</p>
+        <router-link :to="{ name: 'artist', params: { id: artist._id } }" class="btn btn-primary btn-block mt-2">Voir la page</router-link>
       </div>
     </div>
   </div>
 </template>
-<script>
 
+<script>
 export default {
-    name: "ArtistCardForHome",
-    props: {
-      artist: {
-        type: Object,
-        required: true
-      }
+  name: "ArtistCardForHome",
+  props: {
+    artist: {
+      type: Object,
+      required: true
     }
-  };
+  },
+  computed: {
+    totalEcoutes() {
+      if (this.artist && this.artist.albums) {
+        return this.artist.albums.reduce((total, album) => {
+          return total + album.musiques.reduce((sum, music) => sum + (music.nombreEcoutes || 0), 0);
+        }, 0);
+      }
+      return 0;
+    }
+  },
+  methods: {
+    thousandsSeparator(number, separator = ' ') {
+      if (number !== undefined && number !== null) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator);
+      }
+      return '0';
+    }
+  }
+};
 </script>
 
 <style scoped>
